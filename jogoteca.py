006fae609c86eccb5e0aa62f665e3e9a46d032a7
Layra <franelayra@gmail.com>
2022-08-25 20:59:36 -0300

    atualizando arquivo jogoteca

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    lista = [jogo1, jogo2]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
